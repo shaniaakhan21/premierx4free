9d091d4bcd3ea80a66468961b944255727f91b17
Javid <cavid.hacizade.99@gmail.com>
2023-02-09 11:11:13 -0800

    hide NA sections in footer

--- src/components/Footer/Footer.constants.ts
+++ src/components/Footer/Footer.constants.ts
@@ -1,36 +1,36 @@
 import { FooterColumn, FooterSocialShare } from "./Footer.types";
 
 export const FOOTER_ITEMS: FooterColumn[] = [
-	{
-		title: "Company",
-		items: [
-			{
-				title: 'About',
-				link: '/'
-			},
-			{
-				title: 'Meet the Team',
-				link: '/'
-			}
-		]
-	},
-	{
-		title: "Legal",
-		items: [
-			{
-				title: 'Privacy Policy',
-				link: '/'
-			},
-			{
-				title: 'Terms of Use',
-				link: '/'
-			},
-			{
-				title: 'Cookies',
-				link: '/'
-			}
-		]
-	},
+	// {
+	// 	title: "Company",
+	// 	items: [
+	// 		{
+	// 			title: 'About',
+	// 			link: '/'
+	// 		},
+	// 		{
+	// 			title: 'Meet the Team',
+	// 			link: '/'
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	title: "Legal",
+	// 	items: [
+	// 		{
+	// 			title: 'Privacy Policy',
+	// 			link: '/'
+	// 		},
+	// 		{
+	// 			title: 'Terms of Use',
+	// 			link: '/'
+	// 		},
+	// 		{
+	// 			title: 'Cookies',
+	// 			link: '/'
+	// 		}
+	// 	]
+	// },
 	{
 		title: "Connect",
 		items: [
@@ -38,25 +38,25 @@ export const FOOTER_ITEMS: FooterColumn[] = [
 				title: 'Contact Us',
 				link: '/contact'
 			},
-			{
-				title: 'Blog',
-				link: '/'
-			}
+			// {
+			// 	title: 'Blog',
+			// 	link: '/'
+			// }
 		]
 	},
 ]
 
 export const FOOTER_SOCIAL_SHARE_ITEMS: FooterSocialShare[] = [
-	{
-		icon: '/svg/icons/facebook.svg',
-		link: "/"
-	},
-	{
-		icon: '/svg/icons/instagram.svg',
-		link: "/"
-	},
-	{
-		icon: '/svg/icons/twitter.svg',
-		link: "/"
-	}
+	// {
+	// 	icon: '/svg/icons/facebook.svg',
+	// 	link: "/"
+	// },
+	// {
+	// 	icon: '/svg/icons/instagram.svg',
+	// 	link: "/"
+	// },
+	// {
+	// 	icon: '/svg/icons/twitter.svg',
+	// 	link: "/"
+	// }
 ]
