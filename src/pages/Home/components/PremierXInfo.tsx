a43717450ea1764478217b706f02993676c0e778
Javid <cavid.hacizade.99@gmail.com>
2023-02-16 11:41:00 -0800

    fixed typo in premierx info

--- src/pages/Home/components/PremierXInfo.tsx
+++ src/pages/Home/components/PremierXInfo.tsx
@@ -14,13 +14,13 @@ function PremierXInfo(): JSX.Element {
 					<Grid container spacing={4}>
 						<Grid item xs={12} sm={6}>
 							<Typography className={classes.text}>
-								Premierx4Free is a Health and Wellness bussiness sector that allows for Corporations, Unions, Governments, Sole
+								Premierx4Free is a Health and Wellness business sector that allows for Corporations, Unions, Governments, Sole
 								Proprietors and just plan ordinary people to realize significant savings in their prescription and healthcare costs. With our vision and experience in the Health and Wellness area, we are confident that we will be able to provide you with better services than you currently have at a significant saving over what you are currently paying.
 							</Typography>
 						</Grid>
 						<Grid item xs={12} sm={6}>
 							<Typography className={classes.text}>
-								Everything from daily maintenance meds delivered right to your doorstep for free, to calling US based doctors dedicated to helping YOU and having access to a one-of-a-kind prescription benefits that saves you money on your prescriptions (even pet prescriptions) is made available to you through PremierxFree’s Prescription Benefits Management Program.
+								Everything from daily maintenance meds delivered right to your doorstep for free, to calling US based doctors dedicated to helping YOU and having access to a one-of-a-kind prescription benefits that saves you money on your prescriptions (even pet prescriptions) is made available to you through Premierx4Free’s Prescription Benefits Management Program.
 							</Typography>
 						</Grid>
 					</Grid>
