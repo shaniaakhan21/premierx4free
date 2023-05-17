2e1fd87a771f1388393eb845ac41ee31b39dcfc8
amir ali <amiralirang98@gmail.com>
2023-05-17 17:39:28 +0530

    clone of master branch corrected

--- backend/serverRoutes/userModel.js
+++ backend/serverRoutes/userModel.js
@@ -26,25 +26,37 @@ router.post("/signin", async(req,res) => {
 })
 
 router.post("/signup", async(req,res) => {
-    const{email,password}  = req.body
+    const{email,password,fullname,phonenumber,address,city,state,zipcode,referredBy,referralLink}  = req.body
     const data = {
         email:email,
-        password:password
+        password:password,
+        fullname,
+        phonenumber,
+        address,
+        city,
+        state,
+        zipcode,
+        referredBy,
+        referralLink
     }
 
         try{
-            // const check = await findOne({email:email})
+            const check = await userDB.findOne({email:email})
 
-            // if(check){
-            //     res.json("exists")
-            // }
-            // else{
-            //     res.json("notexists")
-            //     await insertMany([data])
-            // }
-            res.send({
-                msg:"signUp route hit"
-            })
+            if(check){
+                res.send({
+                    status:false,
+                    msg:"email Id already exists"
+                })
+            }
+            else{
+                res.json("notexists")
+                await insertMany([data])
+            }
+
+            // res.send({
+            //     msg:"signUp route hit"
+            // })
         }
         catch(e){
             res.json("notexists")
--- frontend/src/admin-dashboard/Admin/components/mainArea_Components/agentDocuments/TableRow.tsx
+++ frontend/src/admin-dashboard/Admin/components/mainArea_Components/agentDocuments/TableRow.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from '../../../../../utils/makeStyles';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {useState} from 'react'
+import React,{useState} from 'react'
 
 interface data {
     agentData:any,
