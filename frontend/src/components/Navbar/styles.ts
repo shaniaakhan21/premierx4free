b6cc90ac16f9fe1246ab24060a78c09225de576f
unknown <shaniaakhan21@gmail.com>
2023-05-19 18:15:03 +0400

    Very Minor Design Fixation

--- frontend/src/components/Navbar/styles.ts
+++ frontend/src/components/Navbar/styles.ts
@@ -2,60 +2,69 @@ import { makeStyles } from "../../utils/makeStyles"
 
 const useStyles = makeStyles()(() => ({
 
-	  container: {
-		padding:'1%!important',
-		maxWidth: '100%!important',
-		margin: '0 15%!important',
-		'@media screen and (max-width: 768px)': {
-			padding:'4%!important',
-			margin: '0 1%!important'
+	container: {
+		padding: '1% !important',
+		maxWidth: '100% !important',
+		margin: '0 15% !important',
+
+		'@media screen and (max-width: 1600px)': {
+			margin: '0 8% !important',
 		},
-	  },
-	  logoContainer: {
+
+		'@media screen and (max-width: 1250px)': {
+			margin: '2% !important',
+		},
+
+		'@media screen and (max-width: 768px)': {
+			padding: '4% !important',
+			margin: '0 1% !important',
+		},
+
+	},
+	logoContainer: {
 		alignSelf: 'flex-start',
 		marginTop: '10px',
 		marginRight: 'auto',
-	  },
-	  logo: {
+	},
+	logo: {
 		backgroundColor: '#0556A7',
 		width: 140,
 		['@media screen and (min-width: 768px)']: {
-		  width: 150,
+			width: 150,
 		},
 		['@media screen and (min-width: 1024px)']: {
-		  width: 160,
+			width: 160,
 		},
 		['@media screen and (min-width: 1280px)']: {
-		  width: 170,
+			width: 170,
 		},
 		['@media screen and (min-width: 1768px)']: {
-		  width: 200,
+			width: 200,
 		},
 		['@media screen and (min-width: 1930px)']: {
-		  width: 215,
+			width: 215,
 		},
-	  },
-	  linkContainer: {
+	},
+	linkContainer: {
 		display: 'flex',
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'flex-end',
-	  },
-	  link: {
+	},
+	link: {
 		color: '#136DA7!important',
 		fontWeight: '400!important',
 		padding: '10px!important',
 		margin: '5px!important',
 		'&:hover': {
-		  color: 'white!important',
-		  backgroundColor: '#64B5F6',
+			color: 'white!important',
+			backgroundColor: '#64B5F6',
 		},
 		'&.active': {
-		  color: 'white!important',
-		  backgroundColor: '#64B5F6',
+			color: 'white!important',
+			backgroundColor: '#64B5F6',
 		},
-	  },
-	}));
-	
-	export default useStyles;
-	
+	},
+}));
+
+export default useStyles;
