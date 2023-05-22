0106dac17bfb074668afb865a30671e5ae49a0a2
amir ali <amiralirang98@gmail.com>
2023-05-22 12:54:14 +0530

    agent-dashboard made protected

--- frontend/src/Dashboard/components/agent-profile/AgentProfile.tsx
+++ frontend/src/Dashboard/components/agent-profile/AgentProfile.tsx
@@ -9,15 +9,15 @@ interface data {
 function AgentProfile(props:data): JSX.Element {
 
     let {agentData} = props
-    console.log("data from agent profile",agentData.data.agentProfile.name)
+    // console.log("data from agent profile",agentData.data.agentProfile.name)
     const classes = useStyles();
 	return (
 		<div>
 			<div className={`${classes.classes.profileBody} "card" `}>
                 <div className={` ${classes.classes.cardCustBody} "card-body" `}>
                     <img className="rounded-circle img-fluid" src={"/assets/svg/Dashboard/dummy.svg"} alt="Card image cap"/>
-                    <h3 className="card-title">{agentData.data.agentProfile.name}</h3>
-                    <span style={{ color:'white!important'}} className="card-text">{agentData.data.email}</span>
+                    <h3 className="card-title">{agentData?.data?.agentProfile?.name}</h3>
+                    <span style={{ color:'white!important'}} className="card-text">{agentData?.data?.email}</span>
                 </div>
             </div>
 		</div>
