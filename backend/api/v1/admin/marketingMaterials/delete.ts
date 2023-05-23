af0b35aab4b78960758afccc4b06b8d87b5c7a1a
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-05-23 10:42:17 +0400

    Update & Delete issues fixed

--- backend/api/v1/admin/marketingMaterials/delete.ts
+++ backend/api/v1/admin/marketingMaterials/delete.ts
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 import { CustomRequestHandler } from '@helpers/errorHandler'
 import { successResponse } from '@helpers/response'
 import MarketingMaterialsModel from '@models/marketingMaterials'
@@ -5,6 +8,12 @@ import MarketingMaterialsModel from '@models/marketingMaterials'
 const deleteMarketingMaterial: CustomRequestHandler<{ id: string }> = async (req, res) => {
   const existing = await MarketingMaterialsModel.findById(req.params.id)
   if (!existing) throw new Error('Category not found')
+  if (existing.document) {
+    const existingFile = path.resolve(__dirname, '../../../../uploads/marketingMaterials', existing.document)
+    if (fs.existsSync(existingFile)) {
+      fs.unlinkSync(existingFile)
+    }
+  }
   await MarketingMaterialsModel.deleteOne({ _id: req.params.id })
 
   res.json(successResponse())
