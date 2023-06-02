481a0f6fa562dea52b15d0efc23bb4bcafcecb16
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-06-02 11:41:49 +0400

    Email credentials added

--- backend/premieRx.pm2.config.js
+++ backend/premieRx.pm2.config.js
@@ -7,7 +7,12 @@ module.exports = {
         NODE_ENV: 'production',
         MONGO_URI:
           'mongodb+srv://adm:dszSgUOIPfa3qD6i@dev-test.d4soidj.mongodb.net/premierx4free_live?retryWrites=true&w=majority',
-        TOKEN_SECRET: 'abc20fd4-c03e-44f1-b05i-cb30e1aa9a60'
+        TOKEN_SECRET: 'abc20fd4-c03e-44f1-b05i-cb30e1aa9a60',
+        EMAIL_HOST: 'smtp.office365.com',
+        EMAIL_PORT: 587,
+        EMAIL_FROM: 'PremieRx4Free <info@premierx4free.com>',
+        EMAIL_USERNAME: 'info@premierx4free.com',
+        EMAIL_PASSWORD: 'rT@6_7cqT$'
       }
     }
   ]
