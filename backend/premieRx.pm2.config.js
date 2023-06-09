cea807c01cfdcd7cdf8145369d20b6fbd00a2a7f
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-06-09 17:07:19 +0400

    Permission issue

--- backend/premieRx.pm2.config.js
+++ backend/premieRx.pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'premierx-backend',
       script: './dist/server.js',
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
         MONGO_URI:
           'mongodb+srv://adm:dszSgUOIPfa3qD6i@dev-test.d4soidj.mongodb.net/premierx4free_live?retryWrites=true&w=majority',
         TOKEN_SECRET: 'abc20fd4-c03e-44f1-b05i-cb30e1aa9a60',
